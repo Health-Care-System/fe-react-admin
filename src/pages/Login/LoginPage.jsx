5c217039730b551016a093d2a5df1b9262fccc25
kiya <Tazkiyalea62@students.unnes.ac.id>
2023-11-13 21:14:38 +0700

    Update Login Admin

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,5 +1,25 @@
+import React from 'react';
+import { LoginForm } from '../../components/ui/Form/LoginForm';
+import Banner from '../../assets/image/banner.png'; 
+import Logo from '../../assets/image/logo.png';// Update the path to your image
+
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="container-fluid" style={{ backgroundColor: '#F8F8F8' }}>
+      <div className="row">
+        <div className="col-md-5 col-lg-7 ">
+          <img src={Banner} alt="Banner" className="img-fluid" />
+        </div>
+        <div className="col-md-5 col-lg-5">
+          <div className=" mt-3 d-flex justify-content-center">
+          <img src={Logo} alt="Logo" className="img-fluid"></img>
+          </div>
+          <div>
+          <LoginForm />
+          </div>
+       
+        </div>
+      </div>
+    </div>
+  );
+};
