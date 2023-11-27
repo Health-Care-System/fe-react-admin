6cc89e8ffacc3e697dc5244d7c67f0b6b719f544
nandorfn <firnandorfn@gmail.com>
2023-11-27 15:04:49 +0700

    update

--- src/services/login-service.js
+++ src/services/login-service.js
@@ -1,5 +1,6 @@
 export const handleLoginError = (error, setErrors) => {
   if (error.response) {
+    console.log(error.response)
     const field = error?.response?.data?.meta?.message;
     switch (error.response.status) {
       case 401:
