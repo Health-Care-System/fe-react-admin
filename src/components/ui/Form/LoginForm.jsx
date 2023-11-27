03e455821af79e5ce6414c8bc5933aa6a3a47c7e
kiya <Tazkiyalea62@students.unnes.ac.id>
2023-11-27 14:49:23 +0700

    Update login form

--- src/components/ui/Form/LoginForm.jsx
+++ src/components/ui/Form/LoginForm.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
-import { Button } from '../Button';
-import Input from './Input'; 
+import { useState } from 'react';
+import Cookies from "js-cookie";
+import { useNavigate } from 'react-router-dom';
+import {
+  handleLoginError,
+  validateFormLogin
+} from '../../../services/login-service';
 import lock from '../../../assets/icon/lock.svg';
 import visibility from '../../../assets/icon/visibility.svg';
+import client from '../../../utils/auth';
+import Input from './Input';
+import { Button } from '../Button';
+import { ErrorMsg } from '../../Errors/ErrorMsg';
+
 
 export const LoginForm = () => {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     email: '',
     password: '',
-    rememberMe: false,
   });
-
-  const [showPassword, setShowPassword] = useState(false);
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+    default: '',
+  });
 
   const togglePasswordVisibility = () => {
     setShowPassword((prev) => !prev);
@@ -20,21 +34,39 @@ export const LoginForm = () => {
   const handleInputChange = (event) => {
     const { name, value, type, checked } = event.target;
     const inputValue = type === 'checkbox' ? checked : value;
-    
+
     setFormData((prevData) => ({
       ...prevData,
       [name]: inputValue,
     }));
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('Form data:', formData);
-   //login untuk submit form data
-  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (validateFormLogin(formData, setErrors)) {
+      try {
+        setLoading(true);
+        const res = await client.post('/admins/login', formData);
+        if (res.status === 200) {
+          const { token } = res.data.results;
+          Cookies.set('token', token);
+          navigate('/')
+        } 
+      } catch (error) {
+        handleLoginError(error, setErrors);
+      } finally {
+        setLoading(false);
+        setFormData({
+          email: '',
+          password: '',
+        })
+      }
+    }
+  }
 
   return (
-    <form action="#" className="mt-4" onSubmit={handleSubmit}>
+    <form className="mt-4" onSubmit={handleSubmit}>
       <div className="mb-3">
         <h3 className="fw-bold text-primary">Portal Admin</h3>
         <p>Kelola informasi kesehatan dengan aman, Silahkan masuk untuk melanjutkan!</p>
@@ -49,6 +81,7 @@ export const LoginForm = () => {
           placeholder="Masukkan email"
           onChange={handleInputChange}
         />
+        <ErrorMsg msg={errors.email} />
       </div>
       <div className="mb-3">
         <label className="form-label fw-medium">Password</label>
@@ -65,6 +98,7 @@ export const LoginForm = () => {
             <img src={visibility} alt={showPassword ? 'hide' : 'show'} />
           </span>
         </div>
+        <ErrorMsg msg={errors.password} />
       </div>
       <div className="d-flex justify-content-between mb-3">
         <div className="form-check p-3 g-col-6">
@@ -74,10 +108,17 @@ export const LoginForm = () => {
           <label className="form-check-label">Lupa Kata Sandi</label>
         </div>
       </div>
-      <div className="mb-3 d-flex flex-column gap-3">
-        <Button type="submit" className="btn btn-primary btn-lg text-light">
-          Masuk
+      <div className="d-flex flex-column gap-3">
+        <Button
+          type="submit"
+          disabled={loading}
+          className="btn-primary btn-lg text-light">
+          {loading
+            ? <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>
+            : 'Masuk'
+          }
         </Button>
+        <ErrorMsg msg={errors.default} />
       </div>
     </form>
   );
