bd4cebecc3f666a9e88ea7e73c7679e705e58ab6
nandorfn <firnandorfn@gmail.com>
2023-12-13 19:31:43 +0700

    fix: add loading simpan btn & fix: bug form medicine validation

--- src/components/Loader/Spinner.jsx
+++ src/components/Loader/Spinner.jsx
@@ -6,3 +6,11 @@ export const Spinner = () => {
 
   )
 }
+export const SpinnerSM = () => {
+  return (
+    <div className="spinner-border spinner-border-sm" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+
+  )
+}
